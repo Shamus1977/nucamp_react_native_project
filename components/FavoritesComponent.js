2dc5caea89d1c1dd342b616dcbd857e7a7e64cb1
SHJohnson <shjohnson413@gmail.com>
2021-08-02 22:40:35 -0700

    Added alert to the delete favorites component

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text, StyleSheet } from 'react-native';
+import { FlatList, View, Text, StyleSheet, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { SwipeRow } from 'react-native-swipe-list-view';
@@ -7,7 +7,6 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 import { deleteFavorite } from '../redux/ActionCreators';
-import { styleSheets } from 'min-document';
 
 const mapStateToProps = state => {
     return {
@@ -36,7 +35,24 @@ class Favorites extends Component{
                     <View style={styles.deleteView}>
                         <TouchableOpacity
                             style={styles.deleteTouchable}
-                            onPress={() => this.props.deleteFavorite(item.id)}
+                            onPress={() =>
+                                Alert.alert(
+                                    'Delete Favorite?',
+                                    'Are you sure you wish to delete the favorite campsite ' + item.name + '?',
+                                    [
+                                        {
+                                            text: 'Cancel',
+                                            onPress: () => console.log(item.name + ' Not Deleted'),
+                                            style: 'cancel'
+                                        },
+                                        {
+                                            text: 'Ok',
+                                            onPress: () => this.props.deleteFavorite(item.id)
+                                        },
+                                    ],
+                                    {cancelable: false}
+                                )
+                            }
                         >
                             <Text style={styles.deleteText} >Delete</Text>
                         </TouchableOpacity>
